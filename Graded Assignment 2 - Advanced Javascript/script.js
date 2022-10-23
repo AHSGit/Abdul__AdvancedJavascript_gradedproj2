dd843fa68ece612bd1108a863e817b80cf993c41
Abdul Hasan Shaikh <98146689+AHSGit@users.noreply.github.com>
2022-10-24 02:01:17 +0530

    Committing Graded Coding Assignment 2 Solution

--- Graded Assignment 2 - Advanced Javascript/script.js
+++ Graded Assignment 2 - Advanced Javascript/script.js
@@ -4,12 +4,12 @@ var state = false;
 function toggle() {
     event.preventDefault();
     if (state) {
-        document.getElementById("password").setAttribute("type", "password");
-        document.getElementById("eye").style.color = '#7a797e';
+        document.querySelector("#password").setAttribute("type", "password");
+        document.querySelector("#eye").style.color = '#7a797e';
         state = false;
     } else {
-        document.getElementById("password").setAttribute("type", "text");
-        document.getElementById("eye").style.color = '#5887ef';
+        document.querySelector("#password").setAttribute("type", "text");
+        document.querySelector("#eye").style.color = '#5887ef';
         state = true;
     }
 }
@@ -21,9 +21,9 @@ localStorage.setItem("password", "admin");
 function validate() {
     event.preventDefault();
 
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var message = document.getElementById("message");
+    var username = document.querySelector("#username").value;
+    var password = document.querySelector("#password").value;
+    var message = document.querySelector("#message");
 
     var storedUsername = localStorage.getItem("username");
     var storedPassword = localStorage.getItem("password");
@@ -48,38 +48,36 @@ history.pushState(null, null, document.URL);
 }); */
 
 
+
 // populate the HTML elements using data from Data.json
 fetch('Data.json')
     .then((res) => res.json())
     .then(data => populatePage(data));
 
-
 var i = 0;
 function populatePage(data) {
-    
+
     displayData();
-    
+
     let prevBtn = document.querySelector("#prev");
     prevBtn.addEventListener("click", () => {
-        if (i > 0) i--;            
-        else i=0;           
+        if (i > 0) i--;
+        else i = 0;
         displayData();
-        console.log(i);
     });
-    
+
     let nextBtn = document.querySelector("#next");
-    nextBtn.addEventListener("click", ()=> {
-        if (i < data.resume.length -1) i++;
-        else i = data.resume.length-1;
+    nextBtn.addEventListener("click", () => {
+        if (i < data.resume.length - 1) i++;
+        else i = data.resume.length - 1;
         displayData();
-        console.log(i);
     });
 
     function displayData() {
-        if (i == 0) document.querySelector("#prev").style.visibility = "hidden";     
+        if (i == 0) document.querySelector("#prev").style.visibility = "hidden";
         else document.querySelector("#prev").style.visibility = "visible";
 
-        if (i == data.resume.length-1) document.querySelector("#next").style.visibility = "hidden";
+        if (i == data.resume.length - 1) document.querySelector("#next").style.visibility = "hidden";
         else document.querySelector("#next").style.visibility = "visible";
 
         // full name
@@ -96,51 +94,52 @@ function populatePage(data) {
         let profileImg = document.querySelector('#img-frame');
         profileImg.innerHTML = `<img src="${data.resume[i].basics.image}" alt="profile-img" id="img" onerror="this.onerror=null;this.src='./profile-icon2.png'">`;
 
-        
+
         // phone number
         let phoneNo = document.querySelector('#phone');
         phoneNo.innerHTML = data.resume[i].basics.phone;
 
-        
+
         // email
         let email = document.querySelector('#email');
         email.innerHTML = data.resume[i].basics.email;
 
-        
+
         // social network details
         let social = document.querySelector('#profile');
         social.innerHTML = `<a href="${data.resume[i].basics.profiles.url}">${data.resume[i].basics.profiles.network}</a>`;
 
-        
+
         // address details
         let address = document.querySelector('#address');
-        address.innerHTML = 
-        `${data.resume[i].basics.location.address},
+        address.innerHTML =
+            `${data.resume[i].basics.location.address},
         ${data.resume[i].basics.location.city},
         ${data.resume[i].basics.location.state},
         ${data.resume[i].basics.location.postalCode}`;
 
-        
+
         // skills list from provided array
         var skillsList = "";
         for (let x = 0; x <= data.resume[i].skills.keywords.length - 1; x++) {
             let skills = data.resume[i].skills.keywords[x];
             skillsList += "<li>" + skills + "</li>";
         }
-       
+
         let skills = document.querySelector('#tech-skills');
         skills.innerHTML = skillsList;
 
-        
+
         // hobbies list from provided array
         var hobbiesList = "";
         for (let y = 0; y <= data.resume[i].interests.hobbies.length - 1; y++) {
             let hobbies = data.resume[i].interests.hobbies[y];
             hobbiesList += "<li>" + hobbies + "</li>";
         }
-        
+
         let hobby = document.querySelector('#hobby');
         hobby.innerHTML = hobbiesList;
+
 
         // experience details
         let company = document.querySelector('#company');
@@ -158,9 +157,55 @@ function populatePage(data) {
         let summary = document.querySelector('#summary');
         summary.innerHTML = data.resume[i].work.Summary;
 
+
         // project details
-        
+        let projectName = document.querySelector('#proj-name');
+        projectName.innerHTML = `${data.resume[i].projects.name}: `;
+
+        let projectDesc = document.querySelector('#proj-desc');
+        projectDesc.innerHTML = data.resume[i].projects.description;
+
+
+        // education details
+        var arr_eduKey = Object.keys(data.resume[i].education);
+        var arr_eduValue = Object.values(data.resume[i].education);
+
+        let edu_UG = `${arr_eduValue[0].institute}, ${arr_eduValue[0].course}, ${arr_eduValue[0]["Start Date"]}, ${arr_eduValue[0]["End Date"]}, ${arr_eduValue[0].cgpa}`;
+        let edu_PU = `${arr_eduValue[1].institute}, ${arr_eduValue[1].cgpa}`;
+        let edu_HS = `${arr_eduValue[2].institute}, ${arr_eduValue[2].cgpa}`;
+
+        var eduList = `<li><span class="highlight">${arr_eduKey[0]}: </span>${edu_UG}</li>
+        <li><span class="highlight">${arr_eduKey[1]}: </span>${edu_PU}</li>
+        <li><span class="highlight">${arr_eduKey[2]}: </span>${edu_HS}</li>`;
+
+        let edu = document.querySelector('#edu');
+        edu.innerHTML = eduList;
+
+
+        // internship details
+        var internList = "";
+        var arr_internKey = Object.keys(data.resume[i].Internship);
+        var arr_internValue = Object.values(data.resume[i].Internship);
+
+        for (let z = 0; z <= arr_internKey.length - 1; z++) {
+            let int_detail = `<span class="highlight">${arr_internKey[z]}: </span>${arr_internValue[z]}`;
+            internList += "<li>" + int_detail + "</li>";
+        }
+
+        let internship = document.querySelector('#intern');
+        internship.innerHTML = internList;
+
+
+        // achievements details
+        var achieveList = "";
+        for (let j = 0; j <= data.resume[i].achievements.Summary.length - 1; j++) {
+            let achievements = data.resume[i].achievements.Summary[j];
+            achieveList += "<li>" + achievements + "</li>";
+        }
+
+        let achievements = document.querySelector('#achieve');
+        achievements.innerHTML = achieveList;
 
     }
-      
+
 };
